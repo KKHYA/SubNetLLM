541c99df8c9cabca8a78aac9240271c11660023a
Mingyuan Zhang <59049356+KKHYA@users.noreply.github.com>
2024-07-15 08:49:57 -0700

    Update globalsubnet.sh

--- out/globalsubnet/globalsubnet.sh
+++ out/globalsubnet/globalsubnet.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+export TRANSFORMERS_CACHE=/sensei-fs/users/yizhouw/cache/
+export HF_HOME=/sensei-fs/users/yizhouw/cache/
+export DEEPSPEED_HOME=/sensei-fs/users/yizhouw/cache/
+export XDG_CACHE_HOME=/sensei-fs/users/yizhouw/cache/
+export TRITON_CACHE_DIR=/sensei-fs/users/yizhouw/cache/
+
 source /home/mingyuan/lab/subnet/bin/activate
 nvidia-smi
 
