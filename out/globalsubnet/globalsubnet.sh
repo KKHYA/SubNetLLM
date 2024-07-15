038c3dbe772c38f18b1a65cd8e3d556211344134
Mingyuan Zhang <59049356+KKHYA@users.noreply.github.com>
2024-07-15 08:53:03 -0700

    Update globalsubnet.sh

--- out/globalsubnet/globalsubnet.sh
+++ out/globalsubnet/globalsubnet.sh
@@ -1,2 +1,5 @@
-cd ../..
+source /home/mingyuan/lab/subnet/bin/activate
+nvidia-smi
+
+cd /work/smile/wanghuan/mz/lit-llama
 python finetune/global_subnet.py --data_dir data/${TASK} --sparsity ${SPAR} --ratio ${RATIO}
