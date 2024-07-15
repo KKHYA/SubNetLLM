0493e9f2b3d978aca386c055f1d302902eb2eabb
Mingyuan Zhang <59049356+KKHYA@users.noreply.github.com>
2024-07-15 08:52:24 -0700

    Update globalsubnet.sh

--- out/globalsubnet/globalsubnet.sh
+++ out/globalsubnet/globalsubnet.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-export TRANSFORMERS_CACHE=/sensei-fs/users/yizhouw/cache/
-export HF_HOME=/sensei-fs/users/yizhouw/cache/
-export DEEPSPEED_HOME=/sensei-fs/users/yizhouw/cache/
-export XDG_CACHE_HOME=/sensei-fs/users/yizhouw/cache/
-export TRITON_CACHE_DIR=/sensei-fs/users/yizhouw/cache/
-
-source /home/mingyuan/lab/subnet/bin/activate
-nvidia-smi
-
-cd /work/smile/wanghuan/mz/lit-llama
+cd ../..
 python finetune/global_subnet.py --data_dir data/${TASK} --sparsity ${SPAR} --ratio ${RATIO}
