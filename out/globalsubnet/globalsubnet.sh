1a0e4b3cab542735035bed8bbfc899a430d4b6a7
Mingyuan Zhang <59049356+KKHYA@users.noreply.github.com>
2024-07-16 19:14:53 -0400

    Update globalsubnet.sh

--- out/globalsubnet/globalsubnet.sh
+++ out/globalsubnet/globalsubnet.sh
@@ -6,8 +6,34 @@ export DEEPSPEED_HOME=/sensei-fs/users/yizhouw/cache/
 export XDG_CACHE_HOME=/sensei-fs/users/yizhouw/cache/
 export TRITON_CACHE_DIR=/sensei-fs/users/yizhouw/cache/
 
-source /home/mingyuan/lab/subnet/bin/activate
-nvidia-smi
+cd
 
-cd /work/smile/wanghuan/mz/lit-llama
-python finetune/global_subnet.py --data_dir data/${TASK} --sparsity ${SPAR} --ratio ${RATIO}
+echo "\n\n ==> Initialize Conda environment..."
+
+source /sensei-fs/users/yizhouw/init_train.sh
+
+echo "Conda initialization completed."
+
+cd /sensei-fs/users/yizhouw/mingyuan/
+
+source work/smile/wanghuan/mz/subnet/bin/activate
+
+# finish conda initialization
+
+echo "\n\n ==> Test Pytorch version..."
+pip show torch
+
+# Set DeepSpeed cache directory
+export DS_CACHE_DIR=/sensei-fs/users/yizhouw/deepspeed_cache/
+mkdir -p $DS_CACHE_DIR
+
+# begin running experiments
+
+
+echo "\n\n ==> cd project..."
+cd /sensei-fs/users/yizhouw/mingyuan/SubNetLLM
+
+echo "\n\n ==> Start runing experiments..."
+
+cd ~/yizhouw/mingyuan/SubNetLLM
+python finetune/global_subnet.py --data_dir data/alpaca --sparsity 0.5 --ratio 1.0
